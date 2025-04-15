6803dd7de684fce97f1e65cca0d8981cd12326af
Raphgarnix <raphael.stude@gmail.com>
2025-04-15 13:28:33 +0200

    MAYBE:::

--- public/canvas.js
+++ public/canvas.js
@@ -1,12 +1,6 @@
 const socket = io();  // Establish connection to the server
 
-var amountOfUser = 0;
 
-const userOnline = document.getElementById('amountOnline');
-
-function updateUsersOnline(){
-  userOnline.textContent = ' Online right now: ' + amountOfUser;
-}
 
 const textbox = document.getElementById('textbox');
 const messagesDiv = document.getElementById('messages'); // Div for displaying messages
@@ -89,8 +83,6 @@ function addUser (userName) {
   if (!currentUsers.includes(userName)) {
     currentUsers.push(userName);
     addDynamicTab(userName); // Call the function to add a tab for the new user
-    amountOfUser += 1;
-    updateUsersOnline();
   }
 }
 
@@ -102,8 +94,6 @@ function removeUser (userName) {
     // Optionally, you can also remove the tab here if needed
     // You might want to implement a way to find and remove the tab
   }
-  amountOfUser -= 1;
-  updateUsersOnline();
 }
 
 // Listen for the updated user list from the server
